8e9bbadfd311f424fdb0dd5702fb76fec79aed1b
Robert Manolea <manolea.robert@gmail.com>
2016-01-04 17:21:48 +0100

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+(function () {
+    var l = document.createElement('link');
+    l.rel = 'import';
+    l.href = 'elements/elements.html;
+    var self = this;
+    document.head.appendChild(l);
+}())
