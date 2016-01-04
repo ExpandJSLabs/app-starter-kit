ed2c1256d9b73291a311101554932cee91215040
Robert Manolea <manolea.robert@gmail.com>
2016-01-04 17:48:50 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,1 @@
-(function () {
-    var d = document.createDocumentFragment();
-    
-    // Polyfill import
-    var s = document.createElement('script');
-    s.type="text/javascript";
-    s.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
-    d.appendChild(s);
-    
-    // Elements import
-    var l = document.createElement('link');
-    l.rel = 'import';
-    l.href = 'elements/elements.html';
-    d.appendChild(l);
-    
-    // Attaching to DOM
-    document.head.appendChild(d);
-}())
+!function(e){var t=e.createDocumentFragment(),n=e.createElement("script");n.type="text/javascript",n.src="bower_components/webcomponentsjs/webcomponents-lite.min.js",t.appendChild(n);var m=e.createElement("link");m.rel="import",m.href="elements/elements.html",t.appendChild(m),e.head.appendChild(t)}(document);
