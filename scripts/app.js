07b55a72fa6f80e0504838cd9c8b675b7d08ac5e
Robert Manolea <manolea.robert@gmail.com>
2016-01-04 17:36:19 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,18 @@
 (function () {
+    var d = document.createDocumentFragment();
+    
+    // Polyfill import
+    var s = document.createElement('script');
+    s.type="text/javascript";
+    s.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
+    d.appendChild(s);
+    
+    // Elements import
     var l = document.createElement('link');
     l.rel = 'import';
-    l.setAttribute('async', '');
     l.href = 'elements/elements.html';
-    document.head.appendChild(l);
+    d.appendChild(l);
+    
+    // Attaching to DOM
+    document.head.appendChild(d);
 }())
