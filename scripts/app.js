de4499602cd864e9667e9f7e5de8d0e108185377
Robert Manolea <manolea.robert@gmail.com>
2016-01-04 17:22:19 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 (function () {
     var l = document.createElement('link');
     l.rel = 'import';
-    l.href = 'elements/elements.html;
+    l.href = 'elements/elements.html';
     var self = this;
     document.head.appendChild(l);
 }())
